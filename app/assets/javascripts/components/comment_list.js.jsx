50ad92d2a9b1770773852d319136a911ae0a26e4
Yoshiyuki Hirano <yhirano@me.com>
2017-05-25 01:21:02 +0900

    Modify CommentList component

--- app/assets/javascripts/components/comment_list.js.jsx
+++ app/assets/javascripts/components/comment_list.js.jsx
@@ -1,7 +1,12 @@
 var CommentList = React.createClass({
-
   render: function() {
-    return <div />;
+    return (
+      <div className="CommentList">
+        <Comment author="alice">My name is Alice.</Comment>
+        <Comment author="bob">My name is Bob.</Comment>
+        <Comment author="carol">My name is Carol.</Comment>
+      </div>
+    );
   }
 });
 
